9d6bb8c515df6a3ffa38cfb5eecc9fb9c2a46f75
Shane BRIGGS <sbriggs@c4r23s1.wethinkcode.co.za>
2019-07-22 15:31:28 +0200

    push1

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //in POM my-app changed to swingy
+        System.out.println( "Hello World!" );
+    }
+}
